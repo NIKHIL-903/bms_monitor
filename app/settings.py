75c8b08801b05c1f8a44747c501624567a506a3c
nikhil <nikhilnaraharisetti7@gmail.com>
2026-03-23 08:36:04 +0530

    updated settings py

--- app/settings.py
+++ app/settings.py
@@ -1,12 +1,13 @@
 import logging
 import os
-
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # optional (for local only)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ALLOWED_USERS = [u.strip() for u in os.getenv("ALLOWED_USERS", "").split(",") if u.strip()]
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+ALLOWED_USERS = os.environ.get("ALLOWED_USERS", "")
+ALLOWED_USERS = [int(u.strip()) for u in ALLOWED_USERS.split(",") if u.strip()]
 
 logging.basicConfig(
     level=logging.INFO,
